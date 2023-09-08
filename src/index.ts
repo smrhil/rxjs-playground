66443efe5d338b83de11128983369c8de23975cb
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-08 18:04:08 +0200

    empty observable

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,7 @@ import { Observable, observable } from 'rxjs';
 
 const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
-    subscriber.next('Alice');
-    setTimeout(() => subscriber.next('Sidi'), 2000) ;
-    setTimeout(() => subscriber.next('Meima'), 4000) ;
 });
-
-const subscription = observable$.subscribe(value => console.log(value));
-
-setTimeout(() => {
-    console.log('Unsubscribe')
-    subscription.unsubscribe()
-}, 3000)
+console.log('Before subscribe');
+observable$.subscribe();
+console.log('After subscribe');
