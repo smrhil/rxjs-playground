336402e2491f4bbb4fd047c3f71b6db113d7d79e
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-08 18:10:16 +0200

    observable qui emet une valeur d une manière synchrone

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { Observable, observable } from 'rxjs';
 
 const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
+    subscriber.next('Alice')
+
 });
 console.log('Before subscribe');
-observable$.subscribe();
+observable$.subscribe(value => console.log(value));
 console.log('After subscribe');
