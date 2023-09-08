ee195a4360a9f144773d0d719233408771dd28a5
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-08 18:31:15 +0200

    handling complete notification + ajout Teardown logic pour le ménage

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
     subscriber.next('Alice');
     subscriber.next('Ben');
-    setTimeout(() => subscriber.next('Charlie'), 2000);
+    setTimeout(() => {
+        subscriber.next('Charlie');
+        subscriber.complete();
+    }, 2000);
+
+    return () => {
+        console.log('Teardown');
+    }
 });
 console.log('Before subscribe');
-observable$.subscribe(value => console.log(value));
+observable$.subscribe(
+    {
+        next: value => console.log(value),
+        complete: () => console.log('Observable completed')   
+    });
 console.log('After subscribe');
