019f5366aa128d61becd50c8ec1ad2b3328c1c02
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-15 15:06:02 +0200

    feat: creation functions, implementing of() creation function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-import { Observable } from 'rxjs';
+import { of, Observable } from "rxjs";
 
-const helloButton = document.querySelector('button#hello');
-
-const helloClick$ = new Observable<MouseEvent>(subscriber => {
-    helloButton.addEventListener('click', (event: MouseEvent) => {
-        subscriber.next(event);
-    });
+ourOwnOf('Alice', 'Ben', 'Charlie').subscribe({
+next: value => console.log(value),
+complete: () => console.log('Completed')
 });
 
-helloClick$.subscribe(
-    event => console.log('Sub 1:', event.type, event.x, event.y)
-)
+/*const names$ = new Observable<string>(subscriber => {
+subscriber.next('Alice');
+subscriber.next('Ben');
+subscriber.next('Charlie');
+subscriber.complete();
+})
 
-setTimeout(() => {
-    helloClick$.subscribe(
-        event => console.log('Sub 2:', event.type, event.x, event.y)
-    )
-}, 5000);
+names$.subscribe({
+      next: value => console.log(value),
+      complete: () => console.log('Completed')
+      });
+*/
+
+function ourOwnOf(...args: string[]): Observable<string> {
+      return new Observable<string>(subscriber => {
+            for(let i=0; i< args.length; i++) {
+                  subscriber.next(args[i]);
+            }
+            subscriber.complete();
+      })
+}
