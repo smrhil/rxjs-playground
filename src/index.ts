bfcc821239aebf16286da124f9ecba1eb70b44e8
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-22 15:36:42 +0200

    feat: tap() pipeable operator to generate a side effect without changing the source observable

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
+import { filter, map, of, tap } from "rxjs";
 
 
-import { forkJoin, map } from "rxjs";
-// Mike is from New Delhi and likes to eat pasta.
-
-import { AjaxResponse, ajax } from "rxjs/ajax";
-
-const randomFirstName$ = ajax<any>('https://random-data-api.com/api/name/random_name').pipe(
-  map(ajaxResponse => ajaxResponse.response.first_name)
-);
-
-const randomCapital$ = ajax<any>('https://random-data-api.com/api/nation/random_nation').pipe(
-  map(ajaxResponse => ajaxResponse.response.capital)
-);;
-
-const randomDish$ = ajax<any>('https://random-data-api.com/api/food/random_food').pipe(
-  map(ajaxResponse => ajaxResponse.response.dish)
-);;
-
-forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
-  ([first_name, capital, dish]) => console.log(`${first_name} is from ${capital} and likes to eat ${dish}.`)
-);
+of(1, 7, 6, 2, 5).pipe(
+  filter(value => value > 5),
+  tap({
+    next: value => console.log('Spy: ', value)
+  }),
+  map(value => value * 2)
+).subscribe(value => console.log('Output:' , value));
