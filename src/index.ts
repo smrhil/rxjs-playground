4816b638ec315a84b0b71804101186aba2f9ecbd
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-22 16:12:40 +0200

    feat: debounce() pipeable operator pour attendre avant de traiter l observable pour éviter des problèmes de perf

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { filter, map, of, tap } from "rxjs";
+import { debounceTime, fromEvent, map } from "rxjs";
+
+const sliderInput = document.querySelector('input#slider');
 
 
-of(1, 7, 6, 2, 5).pipe(
-  filter(value => value > 5),
-  tap({
-    next: value => console.log('Spy: ', value)
-  }),
-  map(value => value * 2)
-).subscribe(value => console.log('Output:' , value));
+fromEvent(sliderInput, 'input').pipe(
+  debounceTime(2000),
+  map((event: any) => event.target['value'])
+).subscribe(event => console.log(event));
