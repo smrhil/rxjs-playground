d2bbc04afaa75c4be268bf8e3096b54b1710402c
smrhil <sidimahmoud.rhil@gmail.com>
2023-09-22 16:30:58 +0200

    feat: catchError() operator pour cacher l erreur ou envoyer une valeur quand l observable plante

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import { debounceTime, fromEvent, map } from "rxjs";
+import { EMPTY, Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-const sliderInput = document.querySelector('input#slider');
 
+const failingHttpRequest$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error(new Error('Timeout'));
+  }, 3000);
+});
 
-fromEvent(sliderInput, 'input').pipe(
-  debounceTime(2000),
-  map((event: any) => event.target['value'])
-).subscribe(event => console.log(event));
+console.log('App started');
+
+// when the observable generates a error notification
+// catchError crée un nouveau observable avec fallback methode and it subscibes to it
+// and sends the result to the output
+failingHttpRequest$.pipe(
+  catchError(error => of('Failing!'))
+).subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed')
+});
+
+// dans certains cas on veut juste cacher l'erreur, on peut utiliser l'observable EMPTY de rxjs 
+// qui complete directement sans envoyer aucune valeur
+
+/*failingHttpRequest$.pipe(
+  catchError(error => EMPTY)
+).subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed')
+});*/
